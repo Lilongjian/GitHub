b510747b92bbd0dedc04f8cebbe33081f4343a62
Lilongjian <34147554+Lilongjian@users.noreply.github.com>
2018-07-24 19:11:18 +0800

    远端修改

--- src/com/centling/Centling.java
+++ src/com/centling/Centling.java
@@ -4,7 +4,7 @@ public class Centling {
 	public static void main(String[] args) {
 		System.out.println("测试数据");
 	}
-	public void name() {
+	public void name34() {
 		System.out.println("这是测试的方法");
 	}
 
